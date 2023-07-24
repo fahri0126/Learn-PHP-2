e18036772d86e14d523613158531fc680ad27c4e
fahri0126 <fahri010206@gmail.com>
2023-07-25 00:42:03 +0800

    menambahkan update dan hapus, memperbaiki operasi crud dari tabel2 yang berelasi

--- belajarrelasi/ubah.php
+++ belajarrelasi/ubah.php
@@ -1,10 +1,26 @@
 <?php
 $koneksi = mysqli_connect("localhost", "root", "", "db_employee");
-if (mysqli_connect_errno()) {
-    die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
+
+$num = $_GET["id"];
+
+$ambil_employee = mysqli_query($koneksi, "SELECT * FROM employee WHERE id=$num");
+$tampil_employee = mysqli_fetch_assoc($ambil_employee);
+$inik = $tampil_employee["id_nik"];
+$id = $tampil_employee["id"];
+
+$tampil_divisi = "SELECT * FROM divisi";
+$result = mysqli_query($koneksi, $tampil_divisi);
+
+$ambilnik = mysqli_query($koneksi, "SELECT * FROM nik WHERE id=$inik");
+$var = mysqli_fetch_assoc($ambilnik);
+
+$ambilprjk = mysqli_query($koneksi, "SELECT * FROM project WHERE project_id=$inik");
+$tampil_prjk = mysqli_fetch_assoc($ambilprjk);
+
+
 
 if (isset($_POST["submit"])) {
+
     $nama = htmlspecialchars($_POST["nama"]);
     $divisi = htmlspecialchars($_POST["id_divisi"]);
     $jk = htmlspecialchars($_POST["jk"]);
@@ -14,36 +30,24 @@ if (isset($_POST["submit"])) {
     $projek = htmlspecialchars($_POST["nama_project"]);
     $deskripsi = htmlspecialchars($_POST["deskripsi"]);
 
-    $tambah_nik = "INSERT INTO nik VALUES ('', '$nik')";
-    mysqli_query($koneksi, $tambah_nik);
-    $id_nik = mysqli_insert_id($koneksi);
+    $query = "UPDATE employee SET 
+                nama = '$nama', id_divisi = '$divisi',  jk = '$jk', foto = '$foto', tgl = '$tgl' WHERE id=$num";
+    $query2 = "UPDATE nik SET 
+                nik = '$nik' WHERE id=$inik";
+    $query3 = "UPDATE project SET 
+                nama_project = '$projek', deskripsi ='$deskripsi' WHERE project_id=$inik";
 
-    $tambah_employee = "INSERT INTO employee VALUES 
-                ('', '$nama', '$jk', '$foto', '$tgl', '$divisi', '$id_nik')";
-    mysqli_query($koneksi, $tambah_employee);
-    $employee_id = mysqli_insert_id($koneksi);
-
-    $tambah_project = "INSERT INTO project VALUES
-                ('$id_nik', '$projek', '$deskripsi')";
-    mysqli_query($koneksi, $tambah_project);
-    $id_project = mysqli_insert_id($koneksi);
-
-    $tambah_ep = "INSERT INTO employee_project (employee_id, project_id) VALUES ('$employee_id', '$id_nik')";
-    mysqli_query($koneksi, $tambah_ep);
-
-    if (mysqli_affected_rows($koneksi) > 0) {
+    $kembali = mysqli_affected_rows($koneksi);
+    mysqli_query($koneksi, $query);
+    mysqli_query($koneksi, $query2);
+    mysqli_query($koneksi, $query3);
+    if ($kembali > 0) {
         header("location:index.php");
-        exit();
-    } else {
-        echo mysqli_error($koneksi);
+        exit;
     }
 }
 
-$tampil_divisi = "SELECT * FROM divisi";
-$result = mysqli_query($koneksi, $tampil_divisi);
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -51,11 +55,11 @@ $result = mysqli_query($koneksi, $tampil_divisi);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tambah Data</title>
+    <title>Update Data</title>
 </head>
 
 <body>
-    <h1>Tambah Data</h1>
+    <h1>UPDATE</h1>
     <form action="" method="post">
         <table>
             <tr>
@@ -63,7 +67,7 @@ $result = mysqli_query($koneksi, $tampil_divisi);
                     <label for="nama">Nama</label>
                 </td>
                 <td>
-                    <input type="text" name="nama" id="nama" autocomplete="off" autofocus required>
+                    <input type="text" name="nama" id="nama" autocomplete="off" autofocus required value="<?= $tampil_employee["nama"]; ?>">
                 </td>
             </tr>
             <tr>
@@ -71,9 +75,7 @@ $result = mysqli_query($koneksi, $tampil_divisi);
                     <label for="jk">JK</label>
                 </td>
                 <td>
-                    <input type="text" name="jk" id="jk" autocomplete="off" required>
-                    <!-- <input type="radio" id="laki" name="jk"><label for="laki" value="Laki-Laki">Laki-Laki</label>
-                    <input type="radio" id="perempuan" name="jk"><label for="perempuan" value="Perempuan">Perempuan</label> -->
+                    <input type="text" name="jk" id="jk" autocomplete="off" required value="<?= $tampil_employee["jk"]; ?>">
                 </td>
             </tr>
             <tr>
@@ -81,7 +83,7 @@ $result = mysqli_query($koneksi, $tampil_divisi);
                     <label for="nik">NIK</label>
                 </td>
                 <td>
-                    <input type="text" id="nik" name="nik" required maxlength="11" pattern="[0-900000000000]{11}" autocomplete="off">
+                    <input type="text" id="nik" name="nik" required maxlength="11" pattern="[0-900000000000]{11}" autocomplete="off" value="<?= $var["nik"]; ?>">
                 </td>
             </tr>
             <tr>
@@ -89,7 +91,7 @@ $result = mysqli_query($koneksi, $tampil_divisi);
                     <label for="foto">Foto</label>
                 </td>
                 <td>
-                    <input type="text" name="foto" id="foto" autocomplete="off" value="noimg.jpg">
+                    <input type="text" name="foto" id="foto" autocomplete="off" value="<?= $tampil_employee["foto"]; ?>">
                 </td>
             </tr>
             <tr>
@@ -107,9 +109,13 @@ $result = mysqli_query($koneksi, $tampil_divisi);
             </tr>
             <tr>
                 <td><label for="projek">Projek</label></td>
-                <td><input type="text" name="nama_project" required id="projek" autocomplete="off"></td>
-                <td><label for="deskripsi">&nbsp;|&nbsp;</label></td>
-                <td><textarea name="deskripsi" id="deskripsi" cols="30" rows="2" autocomplete="off"></textarea></td>
+                <td><input type="text" name="nama_project" required id="projek" autocomplete="off" value="<?= $tampil_prjk["nama_project"]; ?>"></td>
+                <td width="10px" for="deskripsi">
+                    <label for="deskripsi" align="center">
+                        <center>|</center>
+                    </label>
+                </td>
+                <td><textarea name="deskripsi" id="deskripsi" cols="30" rows="2" autocomplete="off"><?= $tampil_prjk["deskripsi"]; ?></textarea></td>
             </tr>
             <tr>
                 <td>
